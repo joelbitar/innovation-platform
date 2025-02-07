40fd15bdfb385ddfe23e725e0e49a966aa02d95e
Joel Shapiro <joel@joelbitar.se>
2025-02-07 10:39:16 +0100

    chore: hide first page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,19 @@
 import Container from "@/app/_components/container";
 import Header from "@/app/_components/header";
 import CampaignList from "@/app/_components/campaign/campaignList";
+import SecuredServer from "@/lib/secureServer";
 
 export default function Index() {
     return (
         <main>
             <Container>
                 <Header/>
-                <h1 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-1 mt-2 flex items-center text-gray-900">
-                    Current campaigns
-                </h1>
-                <CampaignList/>
+                <SecuredServer>
+                    <h1>
+                        Current campaigns
+                    </h1>
+                    <CampaignList/>
+                </SecuredServer>
             </Container>
         </main>
     );
