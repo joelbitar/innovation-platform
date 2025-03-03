f6f9ee4172a09f5e8eceddaad5af0e202dc9af0d
Joel Shapiro <joel@joelbitar.se>
2025-03-03 17:27:19 +0100

    chore: files and stuff

--- backend/file/serializers.py
+++ backend/file/serializers.py
@@ -1,10 +1,50 @@
 from rest_framework import serializers
 
 from file.models import File
+from idea.models import Information
 from user.serializers import CreatedByModelSerializer
+import importlib
 
 
 class FileModelSerializer(CreatedByModelSerializer):
+    related_model = serializers.CharField(required=True, write_only=True)
+    related_pk = serializers.IntegerField(required=True, write_only=True)
+
+    def create(self, validated_data):
+        related_app, related_model = validated_data.pop('related_model').split('.')
+        related_pk = validated_data.pop('related_pk')
+
+        file = super().create(validated_data)
+
+        module = importlib.import_module(f'{related_app}.models')
+
+        cls = getattr(module, related_model)
+
+        obj = cls.objects.get(
+            pk=related_pk
+        )
+
+        obj.file = file
+        obj.save()
+
+        return file
+
+    def create_(self, validated_data):
+        related_app = validated_data.pop('related_model')
+        related_model = validated_data.pop('related_model')
+        related_pk = validated_data.pop('related_pk')
+
+        file = super().create(validated_data)
+
+        obj = Information.objects.get(
+            pk=related_pk
+        )
+
+        obj.file = file
+        obj.save()
+
+        return file
+
     class Meta:
         model = File
         fields = '__all__'
--- backend/file/views.py
+++ backend/file/views.py
@@ -1,7 +1,8 @@
+from rest_framework.generics import GenericAPIView
+
 from file.models import File
 from file.serializers import FileModelSerializer
 from lib.views.created_by_current_user_helper import CreatedByModelViewSet
-
 
 # Create your views here.
 class FileModelViewSet(CreatedByModelViewSet):
@@ -9,3 +10,6 @@ class FileModelViewSet(CreatedByModelViewSet):
 
     serializer_class = FileModelSerializer
     queryset = File.objects.all()
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        return context
